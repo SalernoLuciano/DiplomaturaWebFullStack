7af18672a572609415c90cb0bc037c673dc8288e
Gabriela Tofanelli <gtofanelli009@gmail.com>
2021-08-23 12:03:38 -0300

    Tarea M3 U3 ( Updated )

--- Modulo 3/Tarea M3 U3/ej2.js
+++ Modulo 3/Tarea M3 U3/ej2.js
@@ -2,4 +2,4 @@ var vector = [ Math.random()*10, Math.random()*10, Math.random()*10, Math.random
 
 vector.sort();
 
-document.write( "<br><br>Ejercicio N° 2<br> " + vector[ vector.length - 1 ].toPrecision(2));
+document.write( "<br><br>Ejercicio N° 2<br>Mayor valor del vector: " + vector[ vector.length - 1 ].toPrecision(2));
