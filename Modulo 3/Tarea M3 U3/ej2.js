51e973857c99906c07994bdc80a31f17c9f9d880
Luciano Salerno <lsalerno@fi.uba.ar>
2021-08-25 12:08:28 -0300

    Tarea M3 U3

--- Modulo 3/Tarea M3 U3/ej2.js
+++ Modulo 3/Tarea M3 U3/ej2.js
@@ -1,5 +1,22 @@
-var vector = [ Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10 ];
+// FORMA SIMPLE
+    var vector = [ Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10 ];
+        vector.sort();
+    for(let i = 0; i < vector.length; i++){
+        vector[i] = Math.floor( vector[i] );
+    }
+        document.write( "<br><br>Ejercicio N° 2<br>Mayor valor del vector: " + vector[ vector.length - 1 ]);
 
-vector.sort();
+/* FORMA LARGA
+    // var vector = [ Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11 ];
+    // vector[0] = Math.floor( vector[0] );
+    // var max = vector[0];
 
-document.write( "<br><br>Ejercicio N° 2<br>Mayor valor del vector: " + vector[ vector.length - 1 ].toPrecision(2));
+    // for(let i = 0; i < vector.length; i++){
+    //     vector[i] = Math.floor( vector[i] );
+    //     if( max < vector[i] ){
+    //         max = vector[i];
+    //     }
+    // }
+
+    // document.write( "<br><br>Ejercicio N° 2<br>Mayor valor del vector: " + max);
+*/
