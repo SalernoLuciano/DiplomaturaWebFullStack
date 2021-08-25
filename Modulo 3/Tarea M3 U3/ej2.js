1b8c2ab0b30957bd55500674abc5eb1dda35a045
Luciano Salerno <lsalerno@fi.uba.ar>
2021-08-25 12:11:51 -0300

    Tarea M3 U3

--- Modulo 3/Tarea M3 U3/ej2.js
+++ Modulo 3/Tarea M3 U3/ej2.js
@@ -1,22 +1,21 @@
 // FORMA SIMPLE
-    var vector = [ Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10 ];
+    var vector = [ Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11 ];
         vector.sort();
     for(let i = 0; i < vector.length; i++){
         vector[i] = Math.floor( vector[i] );
     }
-        document.write( "<br><br>Ejercicio N° 2<br>Mayor valor del vector: " + vector[ vector.length - 1 ]);
+    document.write( "<br><br>Ejercicio N° 2 Forma Simple<br>Mayor valor del vector: " + vector[ vector.length - 1 ]);
 
-/* FORMA LARGA
-    // var vector = [ Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11 ];
-    // vector[0] = Math.floor( vector[0] );
-    // var max = vector[0];
+// FORMA LARGA
+    var vector2 = [ Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11, Math.random()*11 ];
+    vector2[0] = Math.floor( vector2[0] );
+    var max = vector2[0];
 
-    // for(let i = 0; i < vector.length; i++){
-    //     vector[i] = Math.floor( vector[i] );
-    //     if( max < vector[i] ){
-    //         max = vector[i];
-    //     }
-    // }
+    for(let i = 0; i < vector2.length; i++){
+        vector2[i] = Math.floor( vector2[i] );
+        if( max < vector2[i] ){
+            max = vector2[i];
+        }
+    }
 
-    // document.write( "<br><br>Ejercicio N° 2<br>Mayor valor del vector: " + max);
-*/
+    document.write( "<br><br>Ejercicio N° 2 Forma Larga<br>Mayor valor del vector: " + max);
