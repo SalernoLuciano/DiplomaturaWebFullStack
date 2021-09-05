5290cd247779b64f95578b73094b823f9fbce1a2
Luciano Salerno <lsalerno@fi.uba.ar>
2021-09-05 01:55:40 -0300

    M3 U4 - Ejercicio 1: OK

---
+++ Modulo 3/Tarea M3 U4/Ejercicio 1/code.js
@@ -0,0 +1,41 @@
+
+const cuerpo = document.getElementsByTagName('body');
+
+const cuadroRojo = document.querySelector('.cuadroRojo');
+const cuadroVerde = document.querySelector('.cuadroVerde');
+const cuadroAzul = document.querySelector('.cuadroAzul');
+
+const botonRojo = document.querySelector('.rojo');
+const botonVerde = document.querySelector('.verde');
+const botonAzul = document.querySelector('.azul');
+
+const reset = document.querySelector('.reset');
+
+
+botonRojo.onclick = function(){
+    cuadroRojo.style.backgroundColor = 'rgb(150, 11, 11)';
+    cuadroVerde.style.backgroundColor = 'rgb(255 255 255)';
+    cuadroAzul.style.backgroundColor = 'rgb(255 255 255)';
+    cuerpo[0].style.backgroundColor = 'rgb(150, 11, 11)';
+};
+
+botonVerde.onclick = function(){
+    cuadroVerde.style.backgroundColor = 'rgb(13 148 13)';
+    cuadroRojo.style.backgroundColor = 'rgb(255 255 255)';
+    cuadroAzul.style.backgroundColor = 'rgb(255 255 255)';
+    cuerpo[0].style.backgroundColor = 'rgb(13 148 13)';
+};
+
+botonAzul.onclick = function(){
+    cuadroAzul.style.backgroundColor = 'rgb(21 21 97)';
+    cuadroRojo.style.backgroundColor = 'rgb(255 255 255)';
+    cuadroVerde.style.backgroundColor = 'rgb(255 255 255)';
+    cuerpo[0].style.backgroundColor = 'rgb(21 21 97)';
+};
+
+reset.onclick = function(){
+    cuadroRojo.style.backgroundColor = 'rgb(255 255 255)';
+    cuadroVerde.style.backgroundColor = 'rgb(255 255 255)';
+    cuadroAzul.style.backgroundColor = 'rgb(255 255 255)';
+    cuerpo[0].style.backgroundColor = 'rgb(255 255 255)';
+};
