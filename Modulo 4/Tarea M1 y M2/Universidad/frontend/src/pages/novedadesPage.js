cd4fe5c1d69067a682516eb56f0d3cde78ccafc1
Luciano Salerno <lsalerno@fi.uba.ar>
2021-10-11 19:37:21 -0300

    Tarea M4U3

--- Modulo 4/Tarea M1 y M2/Universidad/frontend/src/pages/novedadesPage.js
+++ Modulo 4/Tarea M1 y M2/Universidad/frontend/src/pages/novedadesPage.js
@@ -2,61 +2,61 @@ import Clock from '../components/layout/Clock';
 import '../styles/components/pages/novedadesPage.css';
 
 const NovedadesPage = (props) => {
-    return (
-        <main className="container-sm">
-          <Clock/>
-    <h1 className="container-sm"><i className="bi bi-newspaper"></i> News</h1>
-    <div className="separador"></div>
-    <div className="novedad">
-      <h4>Titulo</h4>
-      <h6 className="fecha">Fecha</h6>
-      <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
-        similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
-        animi ratione.</p>
-      <hr/>
-    </div>
-    <div className="novedad">
-      <h4>Titulo</h4>
-      <h6 className="fecha">Fecha</h6>
-      <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
-        similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
-        animi ratione.</p>
-      <hr/>
-    </div>
-    <div className="novedad">
-      <h4>Titulo</h4>
-      <h6 className="fecha">Lunes 15 de Agosto de 2021</h6>
-      <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
-        similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
-        animi ratione.</p>
-      <hr/>
-    </div>
-    <div className="novedad">
-      <h4>Titulo</h4>
-      <h6 className="fecha">Fecha</h6>
-      <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
-        similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
-        animi ratione.</p>
-      <hr/>
-    </div>
-    <div className="novedad">
-      <h4>Titulo</h4>
-      <h6 className="fecha">Fecha</h6>
-      <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
-        similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
-        animi ratione.</p>
-      <hr/>
-    </div>
-    <div className="novedad">
-      <h4>Titulo</h4>
-      <h6 className="fecha">Fecha</h6>
-      <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
-        similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
-        animi ratione.</p>
-      <hr/>
-    </div>
-  </main>
-    );
+  return (
+    <main className="container-sm">
+      <Clock />
+      <h1 className="container-sm"><i className="bi bi-newspaper"></i> News</h1>
+      <div className="separador"></div>
+      <div className="novedad">
+        <h4>Titulo</h4>
+        <h6 className="fecha">Fecha</h6>
+        <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
+          similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
+          animi ratione.</p>
+        <hr />
+      </div>
+      <div className="novedad">
+        <h4>Titulo</h4>
+        <h6 className="fecha">Fecha</h6>
+        <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
+          similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
+          animi ratione.</p>
+        <hr />
+      </div>
+      <div className="novedad">
+        <h4>Titulo</h4>
+        <h6 className="fecha">Lunes 15 de Agosto de 2021</h6>
+        <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
+          similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
+          animi ratione.</p>
+        <hr />
+      </div>
+      <div className="novedad">
+        <h4>Titulo</h4>
+        <h6 className="fecha">Fecha</h6>
+        <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
+          similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
+          animi ratione.</p>
+        <hr />
+      </div>
+      <div className="novedad">
+        <h4>Titulo</h4>
+        <h6 className="fecha">Fecha</h6>
+        <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
+          similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
+          animi ratione.</p>
+        <hr />
+      </div>
+      <div className="novedad">
+        <h4>Titulo</h4>
+        <h6 className="fecha">Fecha</h6>
+        <p>Noticia - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id officiis sequi distinctio dolores
+          similique debitis cupiditate facilis quos eveniet neque mollitia quo eos, quasi iusto incidunt aspernatur ipsum
+          animi ratione.</p>
+        <hr />
+      </div>
+    </main>
+  );
 }
 
 export default NovedadesPage;
