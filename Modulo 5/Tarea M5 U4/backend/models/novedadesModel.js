d59bab8cfc1596354c29b4f5eaff1171f1e16f82
Luciano Salerno <salerno.luciano@outlook.com.ar>
2021-11-09 12:33:54 -0300

    •M6U1 completa •M6U2 Update erroneo

--- Modulo 5/Tarea M5 U4/backend/models/novedadesModel.js
+++ Modulo 5/Tarea M5 U4/backend/models/novedadesModel.js
@@ -1,14 +1,14 @@
 var pool = require('./bd');
 
 async function getNovedades(){
-    var query = "SELECT * FROM novedades ORDER BY id DESC";
+    var query = "select * from novedades order by id desc";
     var rows = await pool.query(query);
     return rows;
 }
 
 async function insertNovedad(obj){
     try{
-        var query = "INSERT INTO novedades SET ?";
+        var query = "insert into novedades set ?";
         var rows = await pool.query(query, [obj]);
         return rows;
     }catch(error){
@@ -17,4 +17,26 @@ async function insertNovedad(obj){
     }
 }
 
-module.exports = {getNovedades, insertNovedad, deleteNovedad}
+async function deleteNovedadById(id){
+    var query = "delete from novedades where id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+
+async function getNovedadById(id){
+    var query = "select * from novedades where id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function updateNovedadById(obj, id){
+    try {
+        var query = "update novedades set ? where id = ?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+module.exports = {getNovedades, insertNovedad, deleteNovedadById, getNovedadById, updateNovedadById}
